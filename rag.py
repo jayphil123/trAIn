50e03e65eecdb67ba68ece6438c709860efd608c
joshbrod <joshbrod@umich.edu>
2024-10-31 17:42:51 -0400

    added comments

--- rag.py
+++ rag.py
@@ -1,24 +1,16 @@
 import os
-from dotenv import load_dotenv
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_chroma import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
-from langchain_openai import ChatOpenAI
 import psycopg2
-from langchain_community.llms import OpenAI
-
-from queue import PriorityQueue
 import numpy as np
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from queue import PriorityQueue
 from numpy.linalg import norm
 
 
 # General Case (user provides user query, return most similar workouts): 
 # Incomplete - currently only provides a list of context exercises. Needs to query GPT with these exercises as contex
 def rag_workouts(query, count):
+    """Returns "count" # of workouts with the closest cosine similarity to the query."""
 
     # Load environment variables from .env file
     load_dotenv()
@@ -31,9 +23,8 @@ def rag_workouts(query, count):
     database_name = os.getenv("DB_NAME")
     openai_key = os.getenv("OPENAI_API_KEY")
 
-
+    # Connect to database
     try:
-        # Connect to the specific database
         connection = psycopg2.connect(
             host=host,
             port=port,
