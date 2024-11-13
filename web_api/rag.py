813b724ac363f4eb15e369d67c9407234103e2c2
joshbrod <joshbrod@umich.edu>
2024-11-13 14:57:55 -0500

    Replaced workout fully working B)

--- web_api/rag.py
+++ web_api/rag.py
@@ -83,7 +83,7 @@ def rag_workouts(query, count=5):
             cosine = -np.dot(query_embedding, workout_embedding)/(norm(query_embedding)*norm(workout_embedding))
             workout_name = embedding[1]
             pq.put((cosine, workout_name))
-
+        
         # Find the top n workouts similar to query
         workouts = []
         for x in range(count):
@@ -253,28 +253,60 @@ def generate_replacement_workout(new_msg: str, conversation_history: dict[str: l
 
     keywords = generate_weekly_workout_keywords(reason, workout_to_be_removed)
     workouts = set()
+   
     for key in keywords:
-        workouts.add(rag_workouts(key))
+        workouts.update(rag_workouts(key))
 
     possible_workouts = get_workout_info(list(workouts))
-
+   
     prompt =  "Your job is to DELETE AND CREATE a new workout based on the conversation history. Unless specified by the chat history \"KEEP\" rest days. "
-    prompt += "Given the Chat History and the Workout's Info let me know what newworkout it should be replaced with. \n"
+    prompt += "Given the Chat History and the Workout's Info let me know what new workout it should be replaced with. \n"
     # prompt += "You are provided the entire routine, if it is not related or applicable to the message or prompt ALWAYS KEEP IT."
-    prompt += "The format should rank the workouts in order of MOST to LEAST applicable as follows:\n"
+    prompt += "The format should rank the workouts in order of MOST to LEAST applicable EXACTLY as follows (it should be json format but as text):\n"
     prompt += "{\"1\":\"workout\", \"2\":\"workout\", ...}\n"
     prompt += "\n\n\n"
     prompt += f"The chat history is: {str(conversation_history)}\n"
     prompt += f"The newest message is from the user is: {str(new_msg)}"
-    prompt += f"The workout you are replacing is {workout_to_be_removed} because {reason}\n"
-    prompt += f"These are other options to replace the above workout {possible_workouts}"
+    prompt += f"The workout you are replacing is {str(workout_to_be_removed)} because {str(reason)}\n"
+    prompt += f"These are other options to replace the above workout {str(possible_workouts)}"
     prompt += "Do not hallucinate or change any of the names of the workouts"
     response = get_chatgpt_response(prompt)
-
+    
     try:
-        response = json.loads(response) # Converts str -> dict
+        response = response.strip()
+        i = response.find("{")
+        j = response.find("}")
+        
+        if i != -1:
+            response = response[i:j+1]
 
+        response = json.loads(response) # Converts str -> dict
+        
         return response, possible_workouts
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
+
+
+def get_time_and_quantity(new_msg: str, conversation_history: dict[str: list[str]], workout: dict[str: str]):
+    prompt =  "Generate an appropriate {\"time\": \"<how long it should take>\", \"quantity\":\"<units appropriate reps/sets, how many miles, etc.>\"} "
+    prompt += f"for the {workout["workout"]} workout given the following stats {workout}\n\n"
+    prompt += f"The following is the chat history for context {conversation_history}"
+    prompt += f"The following is the most recent recieved message for context {new_msg}"
+    response = get_chatgpt_response(prompt)
+    
+    try:
+        response = response.strip()
+        i = response.find("{")
+        j = response.find("}")
+        
+        if i != -1:
+            response = response[i:j+1]
+
+        print(repr(response))
+        response = json.loads(response) # Converts str -> dict
+        
+        return response
     except Exception as e:
         print(f"Error: {e}")
         return {}
@@ -298,13 +330,17 @@ def handle_remove_or_replace(new_msg: str, conversation_history: dict[str: list[
             elif status == "REPLACE":
                 print(f"\tREPLACING {workout["workout"]}")
                 removed_workout = existing_workout[day][i]
-                possible_replacements, workout_info = generate_replacement_workout(new_msg, conversation_history, removed_workout, workout[status])
-                for rank, info in possible_replacements.items():
-                    if info["workout"] == removed_workout["workout"]:
+                possible_replacements, workout_info = generate_replacement_workout(new_msg, conversation_history, removed_workout, reasoning)
+                for rank, name in possible_replacements.items():
+                    if name == removed_workout["workout"]:
                         continue
-                    new_workout[day].append(workout_info[info["workout"]])
+                    new_workout[day].append(workout_info[name])
+                    new_workout[day][-1]["workout"] = name
+                    time_and_quant = get_time_and_quantity(new_msg, conversation_history, new_workout[day][-1])
+                    new_workout[day][-1].update(time_and_quant)
+                    print(f"\tREPLACED with {name}")
                     break
-
+                
             else:
                 new_workout[day].append(existing_workout[day][i])
                 print(f"\tKEEPING {workout["workout"]}")
@@ -342,23 +378,21 @@ def replace_workout_endpoint(new_msg: str, conversation_history: dict[str, list[
     new_workout = handle_remove_or_replace(new_msg, conversation_history, existing_workout_routine, keep_or_delete)
 
 
-    print("NEW:")
-    for day, val in new_workout.items():
-        print(day)
-        for workout in val:
-            print(f"\t{workout["workout"]}")
-
-
 
     return new_workout
 
-# def general_:
+def general_response(new_msg: str, conversation_history: dict[str, list[str]]):
+    prompt =  f"Given this chat history {conversation_history}. "
+    prompt += f"Answer the following prompt {new_msg}."
+    prompt += "ALL RESPONSES MUST BE ABOUT WORKING OUT OR FITNESS. ELSE APOLOGIZE AND ASK FOR A FITNESS RELATED QUESTION."
+
+    return get_chatgpt_response(prompt)
 
 
 def handle_conversation(new_msg: str, conversation_history: dict[str, list[str]]) -> dict:
 
     prompt =  "Your job is to determine which category of messages a message is classified into. "
-    prompt += "Your options are 'New Weekly Workout Routine', 'Replace a Workout/they don't like a workout', 'General Question About Exercise'.\n"
+    prompt += "Your options are 'New Weekly Workout Routine', 'Replace a Workout/They Don't Like a Workout', 'General Question About Exercise'.\n"
     prompt += "Format your response as a json in the following format:\n"
     prompt += "{\"option\": \"<one of the options>\"}\n"
     prompt += "\n\n\n"
@@ -375,7 +409,7 @@ def handle_conversation(new_msg: str, conversation_history: dict[str, list[str]]
             print("New Weekly Workout Routine")
             new_plan = new_weekly_workout_endpoint(new_msg, conversation_history)
             return new_plan, 1
-        if response["option"] == "Replace a Workout/they don't like a workout":
+        if response["option"] == "Replace a Workout/They Don't Like a Workout":
             print("Replace the Current Workout")
             workout_routine = {}
             with open("web_api/example_routine.json", "r") as f:
@@ -384,7 +418,7 @@ def handle_conversation(new_msg: str, conversation_history: dict[str, list[str]]
             return fixed_plan, 2
         if response["option"] == "General Question About Exercise":
             print("General Question About Exercise")
-
+            general_response(new_msg, conversation_history)
             return {}, 3
     except Exception as e:
         print(e)
