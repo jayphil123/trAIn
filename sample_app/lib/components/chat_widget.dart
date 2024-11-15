ec7625c0e917b0ceab3d368519ea133fa63e83c1
Griffin Satterwhite <gsatterw@umich.edu>
2024-11-14 20:20:56 -0500

    added snap scroll button in chat widget

--- sample_app/lib/components/chat_widget.dart
+++ sample_app/lib/components/chat_widget.dart
@@ -34,6 +34,7 @@ class _AIChatWidgetState extends State<AIChatWidget> {
   final List<Message> _messages = [];
   final TextEditingController _controller = TextEditingController();
   final ScrollController _scrollController = ScrollController();
+  bool _isScrolledToBottom = true;
 
   @override
   void initState() {
@@ -43,7 +44,30 @@ class _AIChatWidgetState extends State<AIChatWidget> {
       // Wait for the ListView to finish rendering
       _scrollToBottom();
     });
+
+    _scrollController.addListener(_onScroll);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToBottom();
+    });
     // _clearChatHistory();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.hasClients) {
+      final maxScroll = _scrollController.position.maxScrollExtent;
+      final currentScroll = _scrollController.position.pixels;
+
+      setState(() {
+        _isScrolledToBottom = currentScroll >= (maxScroll - 20);
+      });
+    }
   }
 
   // Method to load messages from local storage
@@ -68,32 +92,31 @@ class _AIChatWidgetState extends State<AIChatWidget> {
     await prefs.setString('messages', messagesJson);
   }
 
-Future<void> _sendMessage() async {
-  if (_controller.text.isNotEmpty) {
-    final userMessage = _controller.text;
+  Future<void> _sendMessage() async {
+    if (_controller.text.isNotEmpty) {
+      final userMessage = _controller.text;
 
-    // Add the user message
-    setState(() {
-      _messages.add(Message(userMessage, true));
-      _messages.add(Message("loading", false));
-    });
-    _controller.clear();
-    _scrollToBottom();
-    _saveMessages();
+      // Add the user message
+      setState(() {
+        _messages.add(Message(userMessage, true));
+        _messages.add(Message("loading", false));
+      });
+      _controller.clear();
+      _scrollToBottom();
+      _saveMessages();
 
-    // Simulate a response with a delay, then update with AI response
-    String aiMessage = await chatMessage(context, userMessage);
+      // Simulate a response with a delay, then update with AI response
+      String aiMessage = await chatMessage(context, userMessage);
 
-    // Add the AI message immediately after receiving it
-    setState(() {
-      _messages.removeLast();
-      _messages.add(Message("AI: $aiMessage", false));
-    });
-    _scrollToBottom();
-    _saveMessages();
+      // Add the AI message immediately after receiving it
+      setState(() {
+        _messages.removeLast();
+        _messages.add(Message("AI: $aiMessage", false));
+      });
+      _scrollToBottom();
+      _saveMessages();
+    }
   }
-}
-
 
   Future<void> _clearChatHistory() async {
     setState(() {
@@ -125,136 +148,144 @@ Future<void> _sendMessage() async {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // This removes the back button
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/images/train-white-logo.png', // Path to your logo image
+              'assets/images/train-white-logo.png',
               width: 110,
               height: 110,
             ),
           ],
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          // Scrollable list of chat messages
-          Expanded(
-              child: Container(
-                  // color: Colors.white,
-                  child: Padding(
-            padding: const EdgeInsets.only(top: 0, bottom: 0),
-            child: ListView.builder(
-              controller: _scrollController,
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                final message = _messages[index];
-                final isUserMessage = message.isUser;
-                final isLoading = message.text == "loading";
+          Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 0, bottom: 0),
+                  child: ListView.builder(
+                    controller: _scrollController,
+                    itemCount: _messages.length,
+                    itemBuilder: (context, index) {
+                      final message = _messages[index];
+                      final isUserMessage = message.isUser;
+                      final isLoading = message.text == "loading";
 
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 12.0),
-                  child: Align(
-                    alignment: isUserMessage
-                        ? Alignment.centerRight
-                        : Alignment.centerLeft,
-                    child: Align(
-                      alignment: isUserMessage
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(
-                          maxWidth: MediaQuery.of(context).size.width * 0.8,
-                        ),
-                        child: isLoading
-                            ? Container(
-                                padding: const EdgeInsets.all(12.0),
-                                decoration: BoxDecoration(
-                                  color: AppTheme.secondaryColor,
-                                  borderRadius: BorderRadius.circular(12),
-                                ),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    const SizedBox(
-                                      width: 20,
-                                      height: 20,
-                                      child: CircularProgressIndicator(
-                                        strokeWidth: 2.0,
-                                        color: Colors.white,
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 12.0),
+                        child: Align(
+                          alignment: isUserMessage
+                              ? Alignment.centerRight
+                              : Alignment.centerLeft,
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(
+                              maxWidth: MediaQuery.of(context).size.width * 0.8,
+                            ),
+                            child: isLoading
+                                ? Container(
+                                    padding: const EdgeInsets.all(12.0),
+                                    decoration: BoxDecoration(
+                                      color: AppTheme.secondaryColor,
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const SizedBox(
+                                          width: 20,
+                                          height: 20,
+                                          child: CircularProgressIndicator(
+                                            strokeWidth: 2.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 8),
+                                        Text(
+                                          "Thinking...",
+                                          style: TextStyle(
+                                              color:
+                                                  AppTheme.bodyTextStyle.color),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                : Container(
+                                    padding: const EdgeInsets.all(12.0),
+                                    decoration: BoxDecoration(
+                                      color: isUserMessage
+                                          ? AppTheme.primaryColor
+                                          : AppTheme.secondaryColor,
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: const Radius.circular(12),
+                                        topRight: const Radius.circular(12),
+                                        bottomLeft: isUserMessage
+                                            ? const Radius.circular(12)
+                                            : const Radius.circular(0),
+                                        bottomRight: isUserMessage
+                                            ? const Radius.circular(0)
+                                            : const Radius.circular(12),
                                       ),
                                     ),
-                                    const SizedBox(width: 8),
-                                    Text(
-                                      "Thinking...",
+                                    child: Text(
+                                      message.text,
                                       style: TextStyle(
                                           color: AppTheme.bodyTextStyle.color),
                                     ),
-                                  ],
-                                ),
-                              )
-                            : Container(
-                                padding: const EdgeInsets.all(12.0),
-                                decoration: BoxDecoration(
-                                  color: isUserMessage
-                                      ? AppTheme.primaryColor
-                                      : AppTheme.secondaryColor,
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: const Radius.circular(12),
-                                    topRight: const Radius.circular(12),
-                                    bottomLeft: isUserMessage
-                                        ? const Radius.circular(12)
-                                        : const Radius.circular(0),
-                                    bottomRight: isUserMessage
-                                        ? const Radius.circular(0)
-                                        : const Radius.circular(12),
                                   ),
-                                ),
-                                child: Text(
-                                  message.text,
-                                  style: TextStyle(
-                                      color: AppTheme.bodyTextStyle.color),
-                                ),
-                              ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ))),
-
-          Container(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-            color: AppTheme.secondaryBackground,
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      hintText: "Type your message...",
-                      border: OutlineInputBorder(),
-                    ),
-                    onSubmitted: (value) async {
-                      await _sendMessage();
+                          ),
+                        ),
+                      );
                     },
                   ),
                 ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () async {
-                    await _sendMessage();
-                  },
-                )
-              ],
-            ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0, vertical: 12.0),
+                color: AppTheme.secondaryBackground,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        decoration: const InputDecoration(
+                          hintText: "Type your message...",
+                          border: OutlineInputBorder(),
+                        ),
+                        onSubmitted: (value) async {
+                          await _sendMessage();
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.send),
+                      onPressed: () async {
+                        await _sendMessage();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
+          if (!_isScrolledToBottom)
+            Positioned(
+              bottom: 60,
+              left: MediaQuery.of(context).size.width / 2 - 25,
+              child: FloatingActionButton(
+                mini: true,
+                backgroundColor: AppTheme.primaryColor,
+                onPressed: _scrollToBottom,
+                child: const Icon(Icons.arrow_downward),
+              ),
+            ),
         ],
       ),
     );
