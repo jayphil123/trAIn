e1defc94be843836bbae1d49e589edf06d62c494
Griffin Satterwhite <gsatterw@umich.edu>
2024-11-14 19:58:53 -0500

    chat auto scrolling working

--- sample_app/lib/chat.dart
+++ sample_app/lib/chat.dart
@@ -17,23 +17,6 @@ class _TrAInChatWidgetState extends State<TrAInChatWidget> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
-        appBar: PreferredSize(
-          preferredSize:
-              Size.fromHeight(MediaQuery.of(context).size.height * 0.05),
-          child: AppBar(
-            automaticallyImplyLeading: false,
-            title: const Text(
-              'Chat with trAIn',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            centerTitle: true,
-            elevation: 2,
-          ),
-        ),
         body: SafeArea(
           child: Column(
             children: const [
--- sample_app/lib/components/chat_widget.dart
+++ sample_app/lib/components/chat_widget.dart
@@ -38,9 +38,11 @@ class _AIChatWidgetState extends State<AIChatWidget> {
   void initState() {
     super.initState();
     _loadMessages();
-    Future.delayed(const Duration(milliseconds: 200), () {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Wait for the ListView to finish rendering
       _scrollToBottom();
     });
+    // _clearChatHistory();
   }
 
   // Method to load messages from local storage
@@ -61,7 +63,7 @@ class _AIChatWidgetState extends State<AIChatWidget> {
     final prefs = await SharedPreferences.getInstance();
     final String messagesJson =
         jsonEncode(_messages.map((msg) => msg.toJson()).toList());
-    print(messagesJson);
+    // print(messagesJson);
     await prefs.setString('messages', messagesJson);
   }
 
@@ -71,6 +73,7 @@ class _AIChatWidgetState extends State<AIChatWidget> {
       final userMessage = _controller.text;
       setState(() {
         _messages.add(Message(userMessage, true));
+        _messages.add(Message("loading", false));
       });
       _controller.clear();
       _scrollToBottom();
@@ -79,6 +82,7 @@ class _AIChatWidgetState extends State<AIChatWidget> {
       // Simulate a response with a delay
       Future.delayed(const Duration(milliseconds: 1000), () {
         setState(() {
+          _messages.removeLast();
           _messages.add(Message("AI: $userMessage", false));
         });
         _scrollToBottom();
@@ -87,28 +91,66 @@ class _AIChatWidgetState extends State<AIChatWidget> {
     }
   }
 
+  Future<void> _clearChatHistory() async {
+    setState(() {
+      _messages.clear(); // Clear the in-memory list
+    });
+
+    // Clear the persisted messages from SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('messages'); // Remove the 'messages' key
+  }
+
   // Automatically scroll to the bottom when a new message is added
   void _scrollToBottom() {
-    _scrollController.animateTo(
-      _scrollController.position.maxScrollExtent,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-    );
+    if (_scrollController.hasClients) {
+      // Use a Future and addPostFrameCallback to ensure the UI is fully built
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Future.delayed(const Duration(milliseconds: 100), () {
+          _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeOut,
+          );
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // This removes the back button
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/train-white-logo.png', // Path to your logo image
+              width: 110,
+              height: 110,
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
           // Scrollable list of chat messages
           Expanded(
+              child: Container(
+                  // color: Colors.white,
+                  child: Padding(
+            padding: const EdgeInsets.only(top: 0, bottom: 0),
             child: ListView.builder(
               controller: _scrollController,
               itemCount: _messages.length,
               itemBuilder: (context, index) {
                 final message = _messages[index];
                 final isUserMessage = message.isUser;
+                final isLoading = message.text == "loading";
+
                 return Padding(
                   padding: const EdgeInsets.symmetric(
                       vertical: 8.0, horizontal: 12.0),
@@ -124,41 +166,67 @@ class _AIChatWidgetState extends State<AIChatWidget> {
                         constraints: BoxConstraints(
                           maxWidth: MediaQuery.of(context).size.width * 0.8,
                         ),
-                        child: Container(
-                          padding: const EdgeInsets.all(12.0),
-                          decoration: BoxDecoration(
-                            color: isUserMessage
-                                ? AppTheme.primaryColor
-                                : AppTheme.secondaryColor,
-                            borderRadius: BorderRadius.only(
-                              topLeft: const Radius.circular(12),
-                              topRight: const Radius.circular(12),
-                              bottomLeft: isUserMessage
-                                  ? const Radius.circular(12)
-                                  : const Radius.circular(0),
-                              bottomRight: isUserMessage
-                                  ? const Radius.circular(0)
-                                  : const Radius.circular(12),
-                            ),
-                          ),
-                          child: Text(
-                            message.text,
-                            style:
-                                TextStyle(color: AppTheme.bodyTextStyle.color),
-                          ),
-                        ),
+                        child: isLoading
+                            ? Container(
+                                padding: const EdgeInsets.all(12.0),
+                                decoration: BoxDecoration(
+                                  color: AppTheme.secondaryColor,
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const SizedBox(
+                                      width: 20,
+                                      height: 20,
+                                      child: CircularProgressIndicator(
+                                        strokeWidth: 2.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Text(
+                                      "Thinking...",
+                                      style: TextStyle(
+                                          color: AppTheme.bodyTextStyle.color),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : Container(
+                                padding: const EdgeInsets.all(12.0),
+                                decoration: BoxDecoration(
+                                  color: isUserMessage
+                                      ? AppTheme.primaryColor
+                                      : AppTheme.secondaryColor,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: const Radius.circular(12),
+                                    topRight: const Radius.circular(12),
+                                    bottomLeft: isUserMessage
+                                        ? const Radius.circular(12)
+                                        : const Radius.circular(0),
+                                    bottomRight: isUserMessage
+                                        ? const Radius.circular(0)
+                                        : const Radius.circular(12),
+                                  ),
+                                ),
+                                child: Text(
+                                  message.text,
+                                  style: TextStyle(
+                                      color: AppTheme.bodyTextStyle.color),
+                                ),
+                              ),
                       ),
                     ),
                   ),
                 );
               },
             ),
-          ),
+          ))),
 
-          // Fixed text input field at the bottom
           Container(
             padding:
-                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
             color: AppTheme.secondaryBackground,
             child: Row(
               children: [
